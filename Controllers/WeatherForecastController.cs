1b0e88f74adc236c207adc89968c95bd61ced1d8
Tomasz Polecki <tomaszpolecki@outlook.com>
2021-04-17 13:20:36 +0100

    Welcome api

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace welcome_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WelcomeController : ControllerBase
+    {        
+        private readonly ILogger<WelcomeController> _logger;
+
+        public WelcomeController(ILogger<WelcomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome to the Api";
+        }
+    }
+}
